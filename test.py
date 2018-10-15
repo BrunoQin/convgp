b89924a72eead98b62f6f2a77fc33c699fd857b8
BrunoQin <brunoqin@163.com>
2018-10-15 21:45:05 +0800

    添加数据和脚本

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+X = np.vstack((mnist.train.images.astype(float), mnist.validation.images.astype('float')))
+Y = np.vstack((np.argmax(mnist.train.labels, 1)[:, None],
+               np.argmax(mnist.validation.labels, 1)[:, None]))
+Xt = mnist.test.images.astype(float)
+Yt = np.argmax(mnist.test.labels, 1)[:, None]
+
+print(X.shape)
