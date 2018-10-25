cab2eceee0592367f9f93630a578858f1b430b59
dl <kris.dacpc@gmail.com>
2018-10-25 22:34:00 +0800

    fix bugs

--- predict-plot.py
+++ predict-plot.py
@@ -5,40 +5,41 @@ import gzip
 import pickle
 
 
-with gzip.open('OCEAN_data/predict.pkl.gz') as fp:
-    predict = np.array(pickle.load(fp)).astype(float)
+# with gzip.open('OCEAN_data/predict.pkl.gz') as fp:
+#     predict = np.array(pickle.load(fp)).astype(float)
+#
+# with gzip.open('OCEAN_data/start.pkl.gz') as fp:
+#     start = np.array(pickle.load(fp)).astype(float)
+#
+#     # clean data
+# start[start == -1.0E20] = 0
+# predict[predict == -1.0E20] = 0
+#
+# nino = np.sum(predict - np.sum(predict, axis=0).reshape(1, predict.shape[1]) / predict.shape[0], axis=1) \
+#            .reshape(predict.shape[0], 1) / predict.shape[1]
+# start = preprocessing.scale(start)
+# ALL = None
+# for i in start:
+#     i.reshape(150, 160)
+#     i = scipy.ndimage.zoom(i.reshape(150, 160), 0.2)
+#     i = i.reshape(1, 960)
+#     if ALL is None:
+#         ALL = i
+#     else:
+#         ALL = np.vstack((ALL, i))
+# X = ALL[0:200]
+# Y = nino[0:200]
+# Xt = ALL[201:300]
+# Yt = nino[201:300]
+#
+# print(Yt)
 
-with gzip.open('OCEAN_data/start.pkl.gz') as fp:
-    start = np.array(pickle.load(fp)).astype(float)
+with gzip.open('OCEAN_data/redata.pkl.gz') as fp:
+    redata = np.array(pickle.load(fp)).astype(float)
 
-    # clean data
-start[start == -1.0E20] = 0
-predict[predict == -1.0E20] = 0
+with gzip.open('OCEAN_data/nino.pkl.gz') as fp:
+    nino = np.array(pickle.load(fp)).astype(float)
 
-nino = np.sum(predict - np.sum(predict, axis=0).reshape(1, predict.shape[1]) / predict.shape[0], axis=1) \
-           .reshape(predict.shape[0], 1) / predict.shape[1]
-start = preprocessing.scale(start)
-ALL = None
-for i in start:
-    i.reshape(150, 160)
-    i = scipy.ndimage.zoom(i.reshape(150, 160), 0.2)
-    i = i.reshape(1, 960)
-    if ALL is None:
-        ALL = i
-    else:
-        ALL = np.vstack((ALL, i))
-X = ALL[0:200]
-Y = nino[0:200]
-Xt = ALL[201:300]
-Yt = nino[201:300]
+print(redata[0:4])
 
-print(Yt)
-
-with gzip.open('results/result.pkl.gz') as fp:
-    pY = np.array(pickle.load(fp)).astype(float)
-
-
-print(pY)
-
-print(np.mean((pY - Yt) ** 2.0) ** 0.5)
-
+print(nino[0:4])
