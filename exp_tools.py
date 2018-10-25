9d7be9b9fe2b792aee05bfb97902a56f5c7e5689
Bruno <brunoqin@163.com>
2018-10-25 23:15:51 +0800

    修改实验数据

--- exp_tools.py
+++ exp_tools.py
@@ -50,21 +50,14 @@ def load_mnist():
 
 def load_ocean():
     # read data
-    with gzip.open('OCEAN_data/predict.pkl.gz') as fp:
-        predict = np.array(pickle.load(fp)).astype(float)
+    with gzip.open('OCEAN_data/redata.pkl.gz') as fp:
+        redata = np.array(pickle.load(fp)).astype(float)
 
-    with gzip.open('OCEAN_data/start.pkl.gz') as fp:
-        start = np.array(pickle.load(fp)).astype(float)
+    with gzip.open('OCEAN_data/index.pkl.gz') as fp:
+        nino = np.array(pickle.load(fp)).astype(float)
 
-    # clean data
-    start[start == -1.0E20] = 0
-    predict[predict == -1.0E20] = 0
-
-    nino = np.sum(predict - np.sum(predict, axis=0).reshape(1, predict.shape[1]) / predict.shape[0], axis=1).\
-               reshape(predict.shape[0], 1) / predict.shape[1]
-    # start = preprocessing.scale(start)
     ALL = None
-    for i in start:
+    for i in redata:
         i.reshape(150, 160)
         i = scipy.ndimage.zoom(i.reshape(150, 160), 0.2)
         i = i.reshape(1, 960)
@@ -72,10 +65,10 @@ def load_ocean():
             ALL = i
         else:
             ALL = np.vstack((ALL, i))
-    X = ALL[0:200]
-    Y = nino[0:200]
-    Xt = ALL[201:300]
-    Yt = nino[201:300]
+    X = ALL[0:4600]
+    Y = nino[11:4611]
+    Xt = ALL[4601:4789]
+    Yt = nino[4612:4800]
     return X, Y, Xt, Yt
 
 
